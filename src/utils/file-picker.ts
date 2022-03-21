6a4b53f6222a310caa73bf779ba58ecb69629662
Daqing <daqing.development21@gmail.com>
2022-03-21 09:23:05 +0800

    Use image-picker instead of document-picker for M1 Mac

--- src/utils/file-picker.ts
+++ src/utils/file-picker.ts
@@ -1,17 +1,19 @@
-import DocumentPicker from 'react-native-document-picker';
-
+import { launchImageLibrary } from 'react-native-image-picker';
 export const pickFile = () => {
   return new Promise((resolve, reject) => {
-    DocumentPicker.pickSingle({
-      type: [DocumentPicker.types.images, DocumentPicker.types.video],
-    })
-      .then(file => {
-        resolve({
-          name: file.name,
-          size: file.size,
-          path: file.uri,
-          type: file.type,
-        });
+    launchImageLibrary({ mediaType: 'mixed' })
+      .then((response) => {
+        if (response.assets && response.assets.length > 0) {
+          const file = response.assets[0]
+          resolve({
+            name: file.fileName,
+            size: file.fileSize,
+            path: file.uri,
+            type: file.type,
+          });
+        } else {
+          reject();
+        }
       })
       .catch(e => {
         reject(e);
